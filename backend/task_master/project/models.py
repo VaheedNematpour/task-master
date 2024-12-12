d0de39ad91f4caea79bd1c4c8e9f7a39c7235ec7
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-12 12:03:51 +0330

    Implement database model for our project

--- backend/task_master/project/models.py
+++ backend/task_master/project/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
