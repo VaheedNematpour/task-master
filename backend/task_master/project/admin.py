12daee68a7338566dd10da06342c612ebce01b38
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-13 11:26:05 +0330

    Implement some issue to the admin panel

--- backend/task_master/project/admin.py
+++ backend/task_master/project/admin.py
@@ -6,11 +6,13 @@ from .models import Project, Task
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
     list_per_page = 10
+    search_fields = ['title__startswith']
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ['id', 'title', 'project']
     list_filter = ['project', 'completed']
     list_per_page = 10
+    list_select_related = ['project']
     search_fields = ['title__startswith']
