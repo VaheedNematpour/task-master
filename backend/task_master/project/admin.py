f4885b1f8524cd408b4d8748115718f2301bb362
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-13 11:18:05 +0330

    Implement admin for project model

--- backend/task_master/project/admin.py
+++ backend/task_master/project/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Project, Task
 
 
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_per_page = 10
