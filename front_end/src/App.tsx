b23162496c4002019a99d49bc3f1d65cbd9c10af
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-15 11:30:24 +0330

    Implement ProjectList component

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import NavBar from "./components/NavBar";
+import ProjectList from "./components/task_master/project_list";
 
 function App() {
   const [dark, setDark] = useState(false);
+  const projects = [
+    { id: 1, title: "prj1" },
+    { id: 2, title: "prj2" },
+    { id: 3, title: "prj3" },
+    { id: 4, title: "prj1" },
+    { id: 5, title: "prj2" },
+    { id: 6, title: "prj3" },
+  ];
 
   return (
     <>
       <header className={`px-6 py-8 ${dark ? "bg-gray-800" : "bg-gray-50"}`}>
         <NavBar isDark={dark} handleDark={() => setDark(!dark)} />
       </header>
-      <main></main>
+      <main className={`px-6 py-8 ${dark ? "bg-gray-800" : "bg-gray-50"}`}>
+        <ProjectList projects={projects} isDark={dark} />
+      </main>
     </>
   );
 }
---
+++ front_end/src/components/task_master/project_list.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+interface Project {
+  id: number;
+  title: string;
+}
+
+interface Props {
+  projects: Project[];
+  isDark: boolean;
+}
+
+function ProjectList({ projects, isDark }: Props) {
+  const [extended, setExtended] = useState(false);
+
+  return (
+    <>
+      <div className="flex items-center justify-between mb-2">
+        <h2
+          className={`text-xl font-medium ${
+            isDark ? "text-gray-300" : "text-gray-800"
+          } xl:text-2xl`}
+        >
+          Projects
+        </h2>
+
+        <button
+          className={`text-md font-bold ${
+            isDark ? "text-gray-300" : "text-gray-800"
+          } xl:text-lg`}
+          onClick={() => setExtended(!extended)}
+        >
+          Show All
+        </button>
+      </div>
+
+      {extended ? (
+        <ul className="grid grid-cols-3">
+          {projects.map((project) => (
+            <li key={project.id}>
+              <article
+                className={`px-4 py-2 ${
+                  isDark ? "bg-gray-700" : "bg-gray-400"
+                } mx-2 my-2 rounded`}
+              >
+                <h3
+                  className={`text-lg font-medium ${
+                    isDark ? "text-gray-300" : "text-gray-800"
+                  } xl:text-2xl`}
+                >
+                  {project.title}
+                </h3>
+              </article>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul className="grid grid-cols-3">
+          {projects.slice(0, 3).map((project) => (
+            <li key={project.id}>
+              <article
+                className={`px-4 py-2 ${
+                  isDark ? "bg-gray-700" : "bg-gray-400"
+                } mx-2 my-2 rounded`}
+              >
+                <h3
+                  className={`text-lg font-medium ${
+                    isDark ? "text-gray-300" : "text-gray-800"
+                  } xl:text-2xl`}
+                >
+                  {project.title}
+                </h3>
+              </article>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default ProjectList;
