73a39ea8d0fa0802165c4e666a936e95c93f92f9
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-21 08:49:31 +0330

    Fix the problem with page's dark mode

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -6,14 +6,14 @@ function App() {
   const [dark, setDark] = useState(false);
 
   return (
-    <>
-      <header className={`px-6 py-8 ${dark ? "bg-gray-800" : "bg-gray-50"}`}>
+    <div className={`min-h-screen ${dark ? "bg-gray-800" : "bg-gray-50"}`}>
+      <header className={`px-6 py-8`}>
         <NavBar isDark={dark} handleDark={() => setDark(!dark)} />
       </header>
-      <main className={`px-6 py-8 ${dark ? "bg-gray-800" : "bg-gray-50"}`}>
+      <main className={`px-6 py-8`}>
         <ProjectList isDark={dark} />
       </main>
-    </>
+    </div>
   );
 }
 
